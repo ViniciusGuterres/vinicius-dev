84229e328fa92180a46609ad0544df2e160c4fc0
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-09-16 20:48:13 -0300

    Create Header.jsx

---
+++ web/src/app/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header class="">
+            <nav
+                class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 border-b-2 border-fuchsia-600"
+                aria-label="Global"
+            >
+                <div class="flex lg:flex-1">
+                    <span>Vinicius Guterre</span>
+                </div>
+            </nav>
+        </header>
+    );
+}
