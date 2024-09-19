0fa17e5489c7882cfac2a74b47cf18a3b05c1ae9
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-09-19 16:29:49 -0300

    Craeted site intro span title, header and call to action

--- web/src/app/components/Header.jsx
+++ web/src/app/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
     return (
-        <header class="">
+        <header className="">
             <nav
-                class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 border-b-2 border-fuchsia-600"
+                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 border-b-2 border-green-600"
                 aria-label="Global"
             >
-                <div class="flex lg:flex-1">
+                <div className="flex lg:flex-1">
                     <span>Vinicius Guterre</span>
                 </div>
             </nav>
