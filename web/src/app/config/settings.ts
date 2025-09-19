f6cf254d21fb91b3f79fd9a8914cebb076371aec
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-09-19 09:14:44 -0300

    Create settings.ts

---
+++ web/src/app/config/settings.ts
@@ -0,0 +1,7 @@
+import { SocialLinks } from "../types";
+
+export const SOCIAL_LINKS: SocialLinks = {
+    "github": "https://github.com/ViniciusGuterres",
+    "linkedin": "https://www.linkedin.com/in/vinicius-carvalho-guterres/", 
+    "email": "viniguter@gmail.com",
+} as const;
