e749b670e323f7f6a717d8f51e226ad32e9bdff8
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-09-29 18:21:53 -0300

    fix: Changed project structure

---
+++ src/app/components/LanguageToggle.tsx
@@ -0,0 +1,7 @@
+export default function LanguageToggle({ }) {
+    return (
+        <div>
+
+        </div>
+    );
+}
---
+++ src/app/contexts/LanguageContext.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { Language, Translations } from "../types"
+import { dictionary } from "../lib/dictionary";
+
+interface LanguageContextType {
+    language: Language;
+    setLanguage: (lang: Language) => void;
+    t: (path: string) => string | string[];
+    isLoading: boolean;
+};
+
+interface LanguageProviderProps {
+    children: ReactNode;
+    initialLanguage?: Language;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+const DEFAULT_LANGUAGE = 'en';
+
+export function LanguageProvider({ children, initialLanguage = DEFAULT_LANGUAGE }: LanguageProviderProps) {
+    const [language, setLanguage] = useState<Language>(initialLanguage);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        // Get language from cookies
+        
+    }, []);
+
+    const t = (path: string): string | string[] => {
+        const keys = path.split('.');
+        let currentDictionary: any = dictionary[language];
+
+        for (const key of keys) {
+            if (currentDictionary && typeof currentDictionary === 'object' && key in currentDictionary) {
+                currentDictionary = currentDictionary[key];
+            } else {
+                console.warn(`Translation not found: ${path} for language: ${language}`);
+                return path;
+            }
+        }
+
+        return currentDictionary;
+    }
+
+    return (
+        <LanguageContext.Provider
+            value={{ language, setLanguage, t, isLoading }}
+        >
+            {children}
+        </LanguageContext.Provider>
+    )
+}
+
+export function useLanguage() {
+    const context = useContext(LanguageContext);
+
+    if (context === undefined) {
+        throw new Error('useLanguage must be used within a LanguageProvider');
+    }
+
+    return context;
+}
